5243983b3280fbc2f2b675cb12a7952394bf7a93
nanu <ranusate19@gmail.com>
2026-01-21 07:09:26 +0800

    update

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,6 +7,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    {{-- icon --}}
+    <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
 
     <!-- Font: Plus Jakarta Sans -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
